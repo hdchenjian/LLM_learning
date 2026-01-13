bd63e1f4be827f8a05e8240ce02b94fac562962e
chenjian <chenjian@example.com>
2026-01-13 14:12:02 +0800

    add bert-pretraining

--- d2l/chapter14_7_bert.py
+++ d2l/chapter14_7_bert.py
@@ -5,64 +5,63 @@ import d2l
 from chapter_9_7_seq2seq import predict_seq2seq, bleu, train_seq2seq
 #os.environ["EN_CN"] = '1'
 
+def get_bert_encoding(net, tokens_a, tokens_b=None):
+    tokens, segments = d2l.get_tokens_and_segments(tokens_a, tokens_b)
+    token_ids = torch.tensor(vocab[tokens], device=devices[0]).unsqueeze(0)
+    segments = torch.tensor(segments, device=devices[0]).unsqueeze(0)
+    valid_len = torch.tensor(len(tokens), device=devices[0]).unsqueeze(0)
+    encoded_X, _, _ = net(token_ids, segments, valid_len)
+    return encoded_X
+
+def train_bert(train_iter, net, loss, vocab_size, devices, num_steps):
+    net = nn.DataParallel(net, device_ids=devices).to(devices[0])
+    trainer = torch.optim.Adam(net.parameters(), lr=0.01)
+    step, timer = 0, d2l.Timer()
+    animator = d2l.Animator(xlabel='step', ylabel='loss', xlim=[1, num_steps], legend=['mlm', 'nsp'])
+    # 遮蔽语言模型损失的和，下一句预测任务损失的和，句子对的数量，计数
+    metric = d2l.Accumulator(4)
+    num_steps_reached = False
+    while step < num_steps and not num_steps_reached:
+        for tokens_X, segments_X, valid_lens_x, pred_positions_X, mlm_weights_X, mlm_Y, nsp_y in train_iter:
+            tokens_X = tokens_X.to(devices[0])
+            segments_X = segments_X.to(devices[0])
+            valid_lens_x = valid_lens_x.to(devices[0])
+            pred_positions_X = pred_positions_X.to(devices[0])
+            mlm_weights_X = mlm_weights_X.to(devices[0])
+            mlm_Y, nsp_y = mlm_Y.to(devices[0]), nsp_y.to(devices[0])
+            trainer.zero_grad()
+            timer.start()
+            mlm_l, nsp_l, l = d2l._get_batch_loss_bert(net, loss, vocab_size, tokens_X, segments_X, valid_lens_x, pred_positions_X, mlm_weights_X, mlm_Y, nsp_y)
+            l.backward()
+            trainer.step()
+            metric.add(mlm_l, nsp_l, tokens_X.shape[0], 1)
+            timer.stop()
+            animator.add(step + 1, (metric[0] / metric[3], metric[1] / metric[3]))
+            step += 1
+            if step == num_steps:
+                num_steps_reached = True
+                break
+
+        print(f'MLM loss {metric[0] / metric[3]:.3f}, ' f'NSP loss {metric[1] / metric[3]:.3f}')
+        print(f'{metric[2] / timer.sum():.1f} sentence pairs/sec on ' f'{str(devices)}')
 
 if __name__ == '__main__':
+    batch_size, max_len = 512, 64
+    train_iter, vocab = d2l.load_data_wiki(batch_size, max_len)
+    net = d2l.BERTModel(len(vocab), num_hiddens=128, norm_shape=[128], ffn_num_input=128, ffn_num_hiddens=256, num_heads=2,
+                        num_layers=2, dropout=0.2, key_size=128, query_size=128, value_size=128, hid_in_features=128, mlm_in_features=128, nsp_in_features=128)
+    devices = d2l.try_all_gpus()
+    loss = nn.CrossEntropyLoss()
 
-    print('len(src_vocab), tgt_vocab', len(src_vocab), len(tgt_vocab))
+    #print('len(src_vocab), tgt_vocab', len(src_vocab), len(tgt_vocab))
     model_path = 'model_14.7_bert.pth'
-    if 1:
-        train_seq2seq(net, train_iter, lr, num_epochs, tgt_vocab, device, src_vocab)
+    if 0:
+        train_bert(train_iter, net, loss, len(vocab), devices, 50)
         torch.save(net.state_dict(), model_path)
-    elif 1:
-        os.environ["TEST_DATA"] = '1'
-        num_examples = 130900
-        source, target  = d2l.load_data_nmt(batch_size, num_steps, num_examples)
-        src_array, src_valid_len = d2l.build_array_nmt(source, src_vocab, num_steps)
-        tgt_array, tgt_valid_len = d2l.build_array_nmt(target, tgt_vocab, num_steps)
-        data_arrays = (src_array[num_examples + 1 - 5000 :], src_valid_len[num_examples + 1 - 5000 :],
-                       tgt_array[num_examples + 1 - 5000 :], tgt_valid_len[num_examples + 1 - 5000 :])
-        test_iter = d2l.load_array(data_arrays, 1, is_train=False)
-
-        print('device', device)
-        device = 'cuda:0'
-        net.load_state_dict(torch.load(model_path, map_location=device))
-        net.to(device)
-        test_num = 0
-        score_sum = 0
-        start = time.time()
-        for batch in test_iter:
-            X, X_valid_len, Y, Y_valid_len = [x.to(device) for x in batch]
-            #print(src_vocab.to_tokens(list(X[0].cpu().numpy())), tgt_vocab.to_tokens(list(Y[0].cpu().numpy())))
-            src_str = ' '.join(src_vocab.to_tokens(list(X[0].cpu().numpy()))).replace('<eos>', '').replace('<pad>', '').strip(' ')
-            if os.getenv("EN_CN", None):
-                target_str = ''.join(tgt_vocab.to_tokens(list(Y[0].cpu().numpy()))).replace('<eos>', '').replace('<pad>', '')
-            else:
-                target_str = ' '.join(tgt_vocab.to_tokens(list(Y[0].cpu().numpy()))).replace('<eos>', '').replace('<pad>', '').strip(' ')
-            #print(src_str, target_str)
-            #import pdb; pdb.set_trace()
-            translation, attention_weight_seq = predict_seq2seq(net, src_str, src_vocab, tgt_vocab, num_steps, device)
-            score = bleu(translation, target_str, k=2)
-            print(f'{src_str} => {translation}, bleu {score:.3f}')
-            test_num += 1
-            score_sum += score
-            #if test_num > 10: break
-        print(f'avg bleu: {score_sum:.4f} / {test_num} = {score_sum / test_num:.4f}, spend {time.time() - start}')
     else:
-        device = 'cpu'
-        net.load_state_dict(torch.load(model_path, map_location='cpu'))
-        engs = ["I know .",
-                'After he had graduated from the university, he taught English for two years .',
-                "According to newspaper reports, there was an airplane accident last evening",
-                "Tom is going to a concert this evening",
-                "These products are of the same quality",
-                "Who are you ?",]
-        fras = ["我知道。",
-                "從他大學畢業以後，他教了兩年的英語。",
-                "根據報載，有一架飛機昨天晚上發生了意外",
-                "汤姆今晚会去演唱会",
-                "这些产品质量同等",
-                "你是谁？",]
-        for eng, fra in zip(engs, fras):
-            translation, attention_weight_seq = predict_seq2seq(net, eng, src_vocab, tgt_vocab, num_steps, device)
-            print(f'{eng} => {translation}, bleu {bleu(translation, fra, k=1):.3f}')
-            #break
+        tokens_a = ['a', 'crane', 'is', 'flying']
+        encoded_text = get_bert_encoding(net, tokens_a)
+        # 词元：'<cls>','a','crane','is','flying','<sep>'
+        encoded_text_cls = encoded_text[:, 0, :]
+        encoded_text_crane = encoded_text[:, 2, :]
+        encoded_text.shape, encoded_text_cls.shape, encoded_text_crane[0][:3]
