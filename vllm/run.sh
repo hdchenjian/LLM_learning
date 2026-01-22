bf32e11423c0a7635b8242fc3e35b95f0ea2f4b3
Your Name <you@example.com>
2026-01-22 17:35:38 +0800

    merge master

--- vllm/run.sh
+++ vllm/run.sh
@@ -1,6 +1,7 @@
 # evalscope[perf]
 if [ 0 -ge 1 ]; then
     #modelscope download --dataset modelscope/gsm8k --local_dir ./data/gsm8k
+    #vllm bench throughput --help
     evalscope eval \
               --model qwen2.5 \
               --api-url http://localhost:8001/v1 --api-key EMPTY --eval-type openai_api \
@@ -11,7 +12,7 @@ elif [ 0 -ge 0 ]; then
     evalscope perf \
               --parallel 1 10 50 \
               --number 10 20 100 \
-              --model qwen2.5 \
+              --model qwen3-0.6b \
               --url http://localhost:8001/v1/chat/completions \
               --api openai \
               --dataset random \
@@ -20,7 +21,7 @@ elif [ 0 -ge 0 ]; then
               --prefix-length 0 \
               --min-prompt-length 1024 \
               --max-prompt-length 1024 \
-              --tokenizer-path qwen2.5-0.5B-Instruct \
+              --tokenizer-path /home/luyao/d/data/llm/qwen/qwen3-0.6b \
               --extra-args '{"ignore_eos": true}'
 
 else
@@ -28,6 +29,8 @@ else
     # V100 GPU does't support bfloat16
     #vllm serve "qwen3-0.6b/" --port 8001 --dtype bfloat16  --max-model-len 512 --gpu-memory-utilization 0.74
     #vllm serve "facebook_opt-125m" --port 8001 --dtype float16 --max-model-len 128 --cpu-offload-gb 10 --max_num_seqs=16 --gpu-memory-utilization 0.7
-    python -m vllm.entrypoints.openai.api_server --model qwen2.5-0.5B-Instruct --port 8001 --dtype bfloat16 --max-model-len 4096 --cpu-offload-gb 10 --max_num_seqs=16 --gpu-memory-utilization 0.7 --served-model-name qwen2.5 --trust-remote-code
+    python -m vllm.entrypoints.openai.api_server --model /home/luyao/d/data/llm/qwen/qwen3-0.6b --port 8001 --dtype bfloat16 --max-model-len 2048 --cpu-offload-gb 10 --max_num_seqs=128 --gpu-memory-utilization 0.95 --served-model-name qwen3-0.6b --trust-remote-code
+    exit
+    python -m vllm.entrypoints.openai.api_server --model qwen2.5-0.5B-Instruct --port 8001 --dtype bfloat16 --max-model-len 1024 --cpu-offload-gb 10 --max_num_seqs=16 --gpu-memory-utilization 0.95 --served-model-name qwen2.5 --trust-remote-code
     #vllm serve "qwen2.5-0.5B-Instruct" --port 8001 --dtype bfloat16 --max-model-len 128 --cpu-offload-gb 10 --max_num_seqs=16 --gpu-memory-utilization 0.73
 fi
