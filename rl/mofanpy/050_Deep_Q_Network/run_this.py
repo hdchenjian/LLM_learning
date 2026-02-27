f160b5b9c67fc97cd7930cb127ecd2d87b46c068
chenjian <chenjian1946@dingtalk.com>
2026-02-27 11:31:39 +0800

    add dqn

--- rl/mofanpy/050_Deep_Q_Network/run_this.py
+++ rl/mofanpy/050_Deep_Q_Network/run_this.py
@@ -5,13 +5,10 @@ from RL_brain import DeepQNetwork
 def run_maze():
     step = 0
     for episode in range(300):
-        # initial observation
         observation = env.reset()
 
         while True:
-            # fresh env
             env.render()
-
             # RL choose action based on observation
             action = RL.choose_action(observation)
 
@@ -23,15 +20,11 @@ def run_maze():
             if (step > 200) and (step % 5 == 0):
                 RL.learn()
 
-            # swap observation
             observation = observation_
-
-            # break while loop when end of this episode
             if done:
                 break
             step += 1
 
-    # end of game
     print('game over')
     env.destroy()
 
