c7e11e0f36cd73e0b6504fd9daf41255468bf858
cj <hdchenjian@163.com>
2026-03-06 16:25:21 +0800

    add seq2seq

--- rl/nlp/tf/cnn-lm.py
+++ rl/nlp/tf/cnn-lm.py
@@ -1,125 +1,119 @@
-# a modification from [Convolutional Neural Networks for Sentence Classification](https://arxiv.org/pdf/1408.5882.pdf)
-
-import tensorflow as tf
-from tensorflow import keras
+from torch import nn
+import torch
 import numpy as np
-import utils    # this refers to utils.py in my [repo](https://github.com/MorvanZhou/NLP-Tutorials/)
-import tensorflow_addons as tfa
+import utils
+from torch.utils.data import DataLoader
+from torch.nn.functional import cross_entropy,softmax, relu
 
 
-class CNNTranslation(keras.Model):
-    def __init__(self, enc_v_dim, dec_v_dim, emb_dim, units, max_pred_len, start_token, end_token):
+
+class CNNTranslation(nn.Module):
+
+    def __init__(self,enc_v_dim, dec_v_dim, emb_dim, units, max_pred_len, start_token, end_token):
         super().__init__()
         self.units = units
+        self.dec_v_dim = dec_v_dim
+
 
         # encoder
-        self.enc_embeddings = keras.layers.Embedding(
-            input_dim=enc_v_dim, output_dim=emb_dim,  # [enc_n_vocab, emb_dim]
-            embeddings_initializer=tf.initializers.RandomNormal(0., 0.1),
-        )
-        self.conv2ds = [
-            keras.layers.Conv2D(16, (n, emb_dim), padding="valid", activation=keras.activations.relu)
-            for n in range(2, 5)]
-        self.max_pools = [keras.layers.MaxPool2D((n, 1)) for n in [7, 6, 5]]
-        self.encoder = keras.layers.Dense(units, activation=keras.activations.relu)
+        self.enc_embeddings = nn.Embedding(enc_v_dim,emb_dim)
+        self.enc_embeddings.weight.data.normal_(0,0.1)
+        self.conv2ds = [nn.Conv2d(1,16,(n,emb_dim),padding=0) for n in range(2,5)]
+        self.max_pools = [nn.MaxPool2d((n,1)) for n in [7,6,5]]
+        self.encoder = nn.Linear(16*3,units)
 
         # decoder
-        self.dec_embeddings = keras.layers.Embedding(
-            input_dim=dec_v_dim, output_dim=emb_dim,  # [dec_n_vocab, emb_dim]
-            embeddings_initializer=tf.initializers.RandomNormal(0., 0.1),
-        )
-        self.decoder_cell = keras.layers.LSTMCell(units=units)
-        decoder_dense = keras.layers.Dense(dec_v_dim)
-        # train decoder
-        self.decoder_train = tfa.seq2seq.BasicDecoder(
-            cell=self.decoder_cell,
-            sampler=tfa.seq2seq.sampler.TrainingSampler(),   # sampler for train
-            output_layer=decoder_dense
-        )
-        # predict decoder
-        self.decoder_eval = tfa.seq2seq.BasicDecoder(
-            cell=self.decoder_cell,
-            sampler=tfa.seq2seq.sampler.GreedyEmbeddingSampler(),       # sampler for predict
-            output_layer=decoder_dense
-        )
+        self.dec_embeddings = nn.Embedding(dec_v_dim,emb_dim)
+        self.dec_embeddings.weight.data.normal_(0,0.1)
+        self.decoder_cell = nn.LSTMCell(emb_dim,units)
+        self.decoder_dense = nn.Linear(units,dec_v_dim)
 
-        self.cross_entropy = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-        self.opt = keras.optimizers.Adam(0.01)
+        self.opt = torch.optim.Adam(self.parameters(),lr=0.001)
         self.max_pred_len = max_pred_len
         self.start_token = start_token
         self.end_token = end_token
+    
+    def encode(self,x):
+        embedded = self.enc_embeddings(x)                   # [n, step, emb]
+        o = torch.unsqueeze(embedded,1)                     # [n, 1, step=8, emb=16]
+        co = [relu(conv2d(o)) for conv2d in self.conv2ds]   # [n, 16, 7, 1], [n, 16, 6, 1], [n, 16, 5, 1]
+        co = [self.max_pools[i](co[i]) for i in range(len(co))]         # [n, 16, 1, 1] * 3
+        co = [torch.squeeze(torch.squeeze(c,dim=3),dim=2) for c in co]  # [n, 16] * 3
+        o = torch.cat(co,dim=1)     # [n, 16*3]
+        h = self.encoder(o)         # [n, units]
+        return [h,h]
+    
+    def inference(self,x):
+        self.eval()
+        hx,cx = self.encode(x)
+        start = torch.ones(x.shape[0],1)
+        start[:,0] = torch.tensor(self.start_token)
+        start= start.type(torch.LongTensor)
+        dec_emb_in = self.dec_embeddings(start) # [n, step, emb]
+        dec_emb_in = dec_emb_in.permute(1,0,2)  # [step, n, emb]
+        dec_in = dec_emb_in[0]  # The first word use for decoding
+        output = []
+        for i in range(self.max_pred_len):
+            hx, cx = self.decoder_cell(dec_in, (hx, cx))
+            o = self.decoder_dense(hx)
+            o = o.argmax(dim=1).view(-1,1)
+            dec_in=self.dec_embeddings(o).permute(1,0,2)[0]
+            output.append(o)
+        output = torch.stack(output,dim=0)  # [self.max_pred_len, n, 1]
+        self.train()
 
-    def encode(self, x):
-        embedded = self.enc_embeddings(x)               # [n, step, emb]
-        o = tf.expand_dims(embedded, axis=3)            # [n, step=8, emb=16, 1]
-        co = [conv2d(o) for conv2d in self.conv2ds]     # [n, 7, 1, 16], [n, 6, 1, 16], [n, 5, 1, 16]
-        co = [self.max_pools[i](co[i]) for i in range(len(co))]     # [n, 1, 1, 16] * 3
-        co = [tf.squeeze(c, axis=[1, 2]) for c in co]               # [n, 16] * 3
-        o = tf.concat(co, axis=1)           # [n, 16*3]
-        h = self.encoder(o)                 # [n, units]
-        return [h, h]
-
-    def inference(self, x):
-        s = self.encode(x)
-        done, i, s = self.decoder_eval.initialize(
-            self.dec_embeddings.variables[0],
-            start_tokens=tf.fill([x.shape[0], ], self.start_token),
-            end_token=self.end_token,
-            initial_state=s,
-        )
-        pred_id = np.zeros((x.shape[0], self.max_pred_len), dtype=np.int32)
-        for l in range(self.max_pred_len):
-            o, s, i, done = self.decoder_eval.step(
-                time=l, inputs=i, state=s, training=False)
-            pred_id[:, l] = o.sample_id
-        return pred_id
-
-    def train_logits(self, x, y, seq_len):
-        s = self.encode(x)
-        dec_in = y[:, :-1]   # ignore <EOS>
+        return output.permute(1,0,2).view(-1,self.max_pred_len) # [n, self.max_pred_len]
+    
+    def train_logit(self,x,y):
+        hx,cx = self.encode(x)  #[n, units]
+        dec_in = y[:,:-1]
         dec_emb_in = self.dec_embeddings(dec_in)
-        o, _, _ = self.decoder_train(dec_emb_in, s, sequence_length=seq_len)
-        logits = o.rnn_output
-        return logits
-
-    def step(self, x, y, seq_len):
-        with tf.GradientTape() as tape:
-            logits = self.train_logits(x, y, seq_len)
-            dec_out = y[:, 1:]  # ignore <GO>
-            loss = self.cross_entropy(dec_out, logits)
-            grads = tape.gradient(loss, self.trainable_variables)
-        self.opt.apply_gradients(zip(grads, self.trainable_variables))
-        return loss.numpy()
+        dec_emb_in = dec_emb_in.permute(1,0,2)
+        output = []
+        for i in range(dec_emb_in.shape[0]):
+            hx, cx = self.decoder_cell(dec_emb_in[i], (hx, cx))
+            o = self.decoder_dense(hx)
+            output.append(o)
+        output = torch.stack(output,dim=0)
+        return output.permute(1,0,2)
+    
+    def step(self,x,y):
+        self.opt.zero_grad()
+        batch_size = x.shape[0]
+        logit = self.train_logit(x,y)    
+        dec_out = y[:,1:]
+        loss = cross_entropy(logit.reshape(-1,self.dec_v_dim),dec_out.reshape(-1))
+        loss.backward()
+        self.opt.step()
+        return loss.detach().numpy()
 
 
 def train():
-    # get and process data
-    data = utils.DateData(4000)
-    print("Chinese time order: yy/mm/dd ", data.date_cn[:3], "\nEnglish time order: dd/M/yyyy ", data.date_en[:3])
-    print("vocabularies: ", data.vocab)
-    print("x index sample: \n{}\n{}".format(data.idx2str(data.x[0]), data.x[0]),
-          "\ny index sample: \n{}\n{}".format(data.idx2str(data.y[0]), data.y[0]))
+    dataset = utils.DateData(4000)
+    print("Chinese time order: yy/mm/dd ",dataset.date_cn[:3],"\nEnglish time order: dd/M/yyyy", dataset.date_en[:3])
+    print("Vocabularies: ", dataset.vocab)
+    print(f"x index sample:  \n{dataset.idx2str(dataset.x[0])}\n{dataset.x[0]}",
+    f"\ny index sample:  \n{dataset.idx2str(dataset.y[0])}\n{dataset.y[0]}")
+    loader = DataLoader(dataset,batch_size=32,shuffle=True)
+    model = CNNTranslation(dataset.num_word,dataset.num_word,emb_dim=16,units=32,max_pred_len=11,start_token=dataset.start_token,end_token=dataset.end_token)
 
-    model = CNNTranslation(
-        data.num_word, data.num_word, emb_dim=16, units=32,
-        max_pred_len=11, start_token=data.start_token, end_token=data.end_token)
-
-    # training
-    for t in range(1500):
-        bx, by, decoder_len = data.sample(32)
-        loss = model.step(bx, by, decoder_len)
-        if t % 70 == 0:
-            target = data.idx2str(by[0, 1:-1])
-            pred = model.inference(bx[0:1])
-            res = data.idx2str(pred[0])
-            src = data.idx2str(bx[0])
-            print(
-                "t: ", t,
-                "| loss: %.3f" % loss,
-                "| input: ", src,
-                "| target: ", target,
-                "| inference: ", res,
-            )
+    for i in range(100):
+        for batch_idx , batch in enumerate(loader):
+            bx, by, decoder_len = batch
+            loss = model.step(bx,by)
+            if batch_idx % 70 == 0:
+                target = dataset.idx2str(by[0, 1:-1].data.numpy())
+                pred = model.inference(bx[0:1])
+                res = dataset.idx2str(pred[0].data.numpy())
+                src = dataset.idx2str(bx[0].data.numpy())
+                print(
+                    "Epoch: ",i,
+                    "| t: ", batch_idx,
+                    "| loss: %.3f" % loss,
+                    "| input: ", src,
+                    "| target: ", target,
+                    "| inference: ", res,
+                )
 
 
 if __name__ == "__main__":
