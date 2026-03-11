cb4c600f4cd6dcadb0f498f2e115bbe346ed4f87
cj <hdchenjian@163.com>
2026-03-11 17:30:53 +0800

    add GPT

--- rl/nlp/tf/BERT.py
+++ rl/nlp/tf/BERT.py
@@ -1,145 +1,151 @@
-# [BERT: Pre-training of Deep Bidirectional Transformers for Language Understanding](https://arxiv.org/pdf/1810.04805.pdf)
+from pickle import load
 import numpy as np
-import tensorflow as tf
-import utils    # this refers to utils.py in my [repo](https://github.com/MorvanZhou/NLP-Tutorials/)
-import time
+from torch import nn
+import torch
+from torch.utils.data import DataLoader
+from torch.nn.functional import cross_entropy,softmax, relu
+
+import utils
 from GPT import GPT
 import os
 import pickle
 
+MASK_RATE = 0.15
 
 class BERT(GPT):
-    def __init__(self, model_dim, max_len, n_layer, n_head, n_vocab, lr, max_seg=3, drop_rate=0.1, padding_idx=0):
-        super().__init__(model_dim, max_len, n_layer, n_head, n_vocab, lr, max_seg, drop_rate, padding_idx)
-        # I think task emb is not necessary for pretraining,
-        # because the aim of all tasks is to train a universal sentence embedding
-        # the body encoder is the same across all tasks,
-        # and different output layer defines different task just like transfer learning.
-        # finetuning replaces output layer and leaves the body encoder unchanged.
 
-        # self.task_emb = keras.layers.Embedding(
-        #     input_dim=n_task, output_dim=model_dim,  # [n_task, dim]
-        #     embeddings_initializer=tf.initializers.RandomNormal(0., 0.01),
-        # )
-
-    def step(self, seqs, segs, seqs_, loss_mask, nsp_labels):
-        with tf.GradientTape() as tape:
-            mlm_logits, nsp_logits = self.call(seqs, segs, training=True)
-            mlm_loss_batch = tf.boolean_mask(self.cross_entropy(seqs_, mlm_logits), loss_mask)
-            mlm_loss = tf.reduce_mean(mlm_loss_batch)
-            nsp_loss = tf.reduce_mean(self.cross_entropy(nsp_labels, nsp_logits))
-            loss = mlm_loss + 0.2 * nsp_loss
-            grads = tape.gradient(loss, self.trainable_variables)
-            self.opt.apply_gradients(zip(grads, self.trainable_variables))
-        return loss, mlm_logits
+    def __init__(
+        self, model_dim, max_len, num_layer, num_head, n_vocab, lr,
+        max_seg=3, drop_rate=0.2, padding_idx=0) -> None:
+        super().__init__(model_dim, max_len, num_layer, num_head, n_vocab, lr, max_seg, drop_rate, padding_idx)
+    
+    def step(self,seqs,segs,seqs_, loss_mask,nsp_labels):
+        device = next(self.parameters()).device
+        self.opt.zero_grad()
+        mlm_logits, nsp_logits = self(seqs, segs, training=True)    # [n, step, n_vocab], [n, n_cls]
+        mlm_loss = cross_entropy(
+            torch.masked_select(mlm_logits,loss_mask).reshape(-1,mlm_logits.shape[2]),
+            torch.masked_select(seqs_,loss_mask.squeeze(2))
+            )
+        nsp_loss = cross_entropy(nsp_logits,nsp_labels.reshape(-1))
+        loss = mlm_loss + 0.2 * nsp_loss
+        loss.backward()
+        self.opt.step()
+        return loss.cpu().data.numpy(),mlm_logits
 
     def mask(self, seqs):
-        mask = tf.cast(tf.math.equal(seqs, self.padding_idx), tf.float32)
-        return mask[:, tf.newaxis, tf.newaxis, :]  # [n, 1, 1, step]
-
+        mask = torch.eq(seqs,self.padding_idx)
+        return mask[:, None, None, :]
 
 def _get_loss_mask(len_arange, seq, pad_id):
     rand_id = np.random.choice(len_arange, size=max(2, int(MASK_RATE * len(len_arange))), replace=False)
-    loss_mask = np.full_like(seq, pad_id, dtype=np.bool)
+    loss_mask = np.full_like(seq, pad_id, dtype=bool)
     loss_mask[rand_id] = True
     return loss_mask[None, :], rand_id
-
 
 def do_mask(seq, len_arange, pad_id, mask_id):
     loss_mask, rand_id = _get_loss_mask(len_arange, seq, pad_id)
     seq[rand_id] = mask_id
     return loss_mask
 
-
 def do_replace(seq, len_arange, pad_id, word_ids):
     loss_mask, rand_id = _get_loss_mask(len_arange, seq, pad_id)
-    seq[rand_id] = np.random.choice(word_ids, size=len(rand_id))
+    seq[rand_id] = torch.from_numpy(np.random.choice(word_ids, size=len(rand_id))).type(torch.IntTensor)
     return loss_mask
-
 
 def do_nothing(seq, len_arange, pad_id):
     loss_mask, _ = _get_loss_mask(len_arange, seq, pad_id)
     return loss_mask
 
-
-def random_mask_or_replace(data, arange, batch_size):
-    seqs, segs, xlen, nsp_labels = data.sample(batch_size)
-    seqs_ = seqs.copy()
+def random_mask_or_replace(data,arange,dataset):
+    seqs, segs,xlen,nsp_labels = data
+    seqs_ = seqs.data.clone()
     p = np.random.random()
     if p < 0.7:
         # mask
-        loss_mask = np.concatenate(
-            [do_mask(
+        loss_mask = np.concatenate([
+            do_mask(
                 seqs[i],
-                np.concatenate((arange[:xlen[i, 0]], arange[xlen[i, 0] + 1:xlen[i].sum() + 1])),
-                data.pad_id,
-                data.v2i["<MASK>"]) for i in range(len(seqs))], axis=0)
+                np.concatenate((arange[:xlen[i,0]],arange[xlen[i,0]+1:xlen[i].sum()+1])),
+                dataset.pad_id,
+                dataset.mask_id
+                )
+                for i in range(len(seqs))], axis=0)
     elif p < 0.85:
         # do nothing
-        loss_mask = np.concatenate(
-            [do_nothing(
+        loss_mask = np.concatenate([
+            do_nothing(
                 seqs[i],
-                np.concatenate((arange[:xlen[i, 0]], arange[xlen[i, 0] + 1:xlen[i].sum() + 1])),
-                data.pad_id) for i in range(len(seqs))], axis=0)
+                np.concatenate((arange[:xlen[i,0]],arange[xlen[i,0]+1:xlen[i].sum()+1])),
+                dataset.pad_id
+                )
+                for i in range(len(seqs))],  axis=0)
     else:
         # replace
-        loss_mask = np.concatenate(
-            [do_replace(
+        loss_mask = np.concatenate([
+            do_replace(
                 seqs[i],
-                np.concatenate((arange[:xlen[i, 0]], arange[xlen[i, 0] + 1:xlen[i].sum() + 1])),
-                data.pad_id,
-                data.word_ids) for i in range(len(seqs))], axis=0)
+                np.concatenate((arange[:xlen[i,0]],arange[xlen[i,0]+1:xlen[i].sum()+1])),
+                dataset.pad_id,
+                dataset.word_ids
+                )
+                for i in range(len(seqs))],  axis=0)
+    loss_mask = torch.from_numpy(loss_mask).unsqueeze(2)
     return seqs, segs, seqs_, loss_mask, xlen, nsp_labels
 
-
-def train(model, data, step=10000, name="bert"):
-    t0 = time.time()
-    arange = np.arange(0, data.max_len)
-    for t in range(step):
-        seqs, segs, seqs_, loss_mask, xlen, nsp_labels = random_mask_or_replace(data, arange, 16)
-        loss, pred = model.step(seqs, segs, seqs_, loss_mask, nsp_labels)
-        if t % 100 == 0:
-            pred = pred[0].numpy().argmax(axis=1)
-            t1 = time.time()
-            print(
-                "\n\nstep: ", t,
-                "| time: %.2f" % (t1 - t0),
-                "| loss: %.3f" % loss.numpy(),
-                "\n| tgt: ", " ".join([data.i2v[i] for i in seqs[0][:xlen[0].sum()+1]]),
-                "\n| prd: ", " ".join([data.i2v[i] for i in pred[:xlen[0].sum()+1]]),
-                "\n| tgt word: ", [data.i2v[i] for i in seqs_[0]*loss_mask[0] if i != data.v2i["<PAD>"]],
-                "\n| prd word: ", [data.i2v[i] for i in pred*loss_mask[0] if i != data.v2i["<PAD>"]],
+def train():
+    MODEL_DIM = 256
+    N_LAYER = 4
+    LEARNING_RATE = 1e-4
+    dataset = utils.MRPCData("./MRPC",2000)
+    print("num word: ",dataset.num_word)
+    model = BERT(
+        model_dim=MODEL_DIM, max_len=dataset.max_len, num_layer=N_LAYER, num_head=4, n_vocab=dataset.num_word,
+        lr=LEARNING_RATE, max_seg=dataset.num_seg, drop_rate=0.2, padding_idx=dataset.pad_id
+    )
+    if torch.cuda.is_available():
+        print("GPU train avaliable")
+        device =torch.device("cuda")
+        model = model.cuda()
+    else:
+        device = torch.device("cpu")
+        model = model.cpu()
+    
+    loader = DataLoader(dataset,batch_size=32,shuffle=True)
+    arange = np.arange(0,dataset.max_len)
+    for epoch in range(500):
+        for batch_idx, batch in enumerate(loader):
+            seqs, segs, seqs_, loss_mask, xlen, nsp_labels = random_mask_or_replace(batch,arange,dataset)
+            seqs, segs, seqs_, nsp_labels, loss_mask = seqs.type(torch.LongTensor).to(device), segs.type(torch.LongTensor).to(device), \
+                seqs_.type(torch.LongTensor).to(device),nsp_labels.to(device),loss_mask.to(device)
+            loss, pred = model.step(seqs, segs, seqs_, loss_mask, nsp_labels)
+            if batch_idx % 100 == 0:
+                pred = pred[0].cpu().data.numpy().argmax(axis=1)
+                print(
+                "\n\nEpoch: ",epoch,
+                "|batch: ", batch_idx,
+                "| loss: %.3f" % loss,
+                "\n| tgt: ", " ".join([dataset.i2v[i] for i in seqs[0].cpu().data.numpy()[:xlen[0].sum()+1]]),
+                "\n| prd: ", " ".join([dataset.i2v[i] for i in pred[:xlen[0].sum()+1]]),
+                "\n| tgt word: ", [dataset.i2v[i] for i in (seqs_[0]*loss_mask[0].view(-1)).cpu().data.numpy() if i != dataset.v2i["<PAD>"]],
+                "\n| prd word: ", [dataset.i2v[i] for i in pred*(loss_mask[0].view(-1).cpu().data.numpy()) if i != dataset.v2i["<PAD>"]],
                 )
-            t0 = t1
-    os.makedirs("./visual/models/%s" % name, exist_ok=True)
-    model.save_weights("./visual/models/%s/model.ckpt" % name)
+    os.makedirs("./visual/models/bert",exist_ok=True)
+    torch.save(model.state_dict(),"./visual/models/bert/model.pth")
+    export_attention(model,device,dataset)
 
-
-def export_attention(model, data, name="bert"):
-    model.load_weights("./visual/models/%s/model.ckpt" % name)
-
-    # save attention matrix for visualization
-    seqs, segs, xlen, nsp_labels = data.sample(32)
-    model.call(seqs, segs, False)
+def export_attention(model,device,data,name="bert"):
+    model.load_state_dict(torch.load("./visual/models/bert/model.pth",map_location=device))
+    seqs, segs,xlen,nsp_labels = data[:32]
+    seqs, segs,xlen,nsp_labels = torch.from_numpy(seqs),torch.from_numpy(segs),torch.from_numpy(xlen),torch.from_numpy(nsp_labels)
+    seqs, segs,nsp_labels = seqs.type(torch.LongTensor).to(device), segs.type(torch.LongTensor).to(device),nsp_labels.to(device)
+    model(seqs,segs,False)
+    seqs = seqs.cpu().data.numpy()
     data = {"src": [[data.i2v[i] for i in seqs[j]] for j in range(len(seqs))], "attentions": model.attentions}
     path = "./visual/tmp/%s_attention_matrix.pkl" % name
     os.makedirs(os.path.dirname(path), exist_ok=True)
     with open(path, "wb") as f:
         pickle.dump(data, f)
 
-
 if __name__ == "__main__":
-    utils.set_soft_gpu(True)
-    MODEL_DIM = 256
-    N_LAYER = 4
-    LEARNING_RATE = 1e-4
-    MASK_RATE = 0.15
-
-    d = utils.MRPCData("./MRPC", 2000)
-    print("num word: ", d.num_word)
-    m = BERT(
-        model_dim=MODEL_DIM, max_len=d.max_len, n_layer=N_LAYER, n_head=4, n_vocab=d.num_word,
-        lr=LEARNING_RATE, max_seg=d.num_seg, drop_rate=0.2, padding_idx=d.v2i["<PAD>"])
-    train(m, d, step=10000, name="bert")
-    export_attention(m, d, "bert")
-
+    train()
--- rl/nlp/tf/GPT.py
+++ rl/nlp/tf/GPT.py
@@ -1,143 +1,129 @@
-# [Improving Language Understanding by Generative Pre-Training](https://cdn.openai.com/research-covers/language-unsupervised/language_understanding_paper.pdf)
-import tensorflow as tf
-from tensorflow import keras
-import utils    # this refers to utils.py in my [repo](https://github.com/MorvanZhou/NLP-Tutorials/)
-import time
 from transformer import Encoder
-import pickle
+from torch import nn,optim
+from torch.nn.functional import cross_entropy,softmax, relu
+from torch.utils.data import DataLoader
+from torch.utils.data.dataloader import default_collate
+
+import torch
+import utils
 import os
+import pickle
 
+class GPT(nn.Module):
 
-class GPT(keras.Model):
-    def __init__(self, model_dim, max_len, n_layer, n_head, n_vocab, lr, max_seg=3, drop_rate=0.1, padding_idx=0):
+    def __init__(self, model_dim, max_len, num_layer, num_head, n_vocab, lr, max_seg=3, drop_rate=0.2,padding_idx=0):
         super().__init__()
         self.padding_idx = padding_idx
         self.n_vocab = n_vocab
         self.max_len = max_len
+        
+        self.word_emb = nn.Embedding(n_vocab,model_dim)
+        self.word_emb.weight.data.normal_(0,0.1)
 
-        # I think task emb is not necessary for pretraining,
-        # because the aim of all tasks is to train a universal sentence embedding
-        # the body encoder is the same across all tasks,
-        # and different output layer defines different task just like transfer learning.
-        # finetuning replaces output layer and leaves the body encoder unchanged.
+        self.segment_emb = nn.Embedding(num_embeddings= max_seg, embedding_dim=model_dim)
+        self.segment_emb.weight.data.normal_(0,0.1)
+        self.position_emb = torch.empty(1,max_len,model_dim)
+        nn.init.kaiming_normal_(self.position_emb,mode='fan_out', nonlinearity='relu')
+        self.position_emb = nn.Parameter(self.position_emb)
 
-        # self.task_emb = keras.layers.Embedding(
-        #     input_dim=n_task, output_dim=model_dim,  # [n_task, dim]
-        #     embeddings_initializer=tf.initializers.RandomNormal(0., 0.01),
-        # )
 
-        self.word_emb = keras.layers.Embedding(
-            input_dim=n_vocab, output_dim=model_dim,  # [n_vocab, dim]
-            embeddings_initializer=tf.initializers.RandomNormal(0., 0.01),
-        )
-        self.segment_emb = keras.layers.Embedding(
-            input_dim=max_seg, output_dim=model_dim,  # [max_seg, dim]
-            embeddings_initializer=tf.initializers.RandomNormal(0., 0.01),
-        )
-        self.position_emb = self.add_weight(
-            name="pos", shape=[1, max_len, model_dim], dtype=tf.float32,   # [1, step, dim]
-            initializer=keras.initializers.RandomNormal(0., 0.01))
-        self.encoder = Encoder(n_head, model_dim, drop_rate, n_layer)
-        self.task_mlm = keras.layers.Dense(n_vocab)
-        self.task_nsp = keras.layers.Dense(2)
+        self.encoder = Encoder(n_head=num_head, emb_dim=model_dim, drop_rate=drop_rate, n_layer=num_layer)
+        self.task_mlm = nn.Linear(in_features=model_dim, out_features=n_vocab)
+        self.task_nsp = nn.Linear(in_features=model_dim*self.max_len, out_features=2)
 
-        self.cross_entropy = keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction="none")
-        self.opt = keras.optimizers.Adam(lr)
-
-    def call(self, seqs, segs, training=False):
-        embed = self.input_emb(seqs, segs)  # [n, step, dim]
-        z = self.encoder(embed, training=training, mask=self.mask(seqs))     # [n, step, dim]
-        mlm_logits = self.task_mlm(z)  # [n, step, n_vocab]
-        nsp_logits = self.task_nsp(tf.reshape(z, [z.shape[0], -1]))  # [n, n_cls]
+        self.opt = optim.Adam(self.parameters(),lr)
+    
+    def forward(self,seqs, segs, training=False):
+        embed = self.input_emb(seqs, segs)
+        z = self.encoder(embed, training, mask = self.mask(seqs))   # [n, step, model_dim]
+        mlm_logits = self.task_mlm(z)   # [n, step, n_vocab]
+        nsp_logits = self.task_nsp(z.reshape(z.shape[0],-1))    # [n, n_cls]
         return mlm_logits, nsp_logits
-
+    
     def step(self, seqs, segs, seqs_, nsp_labels):
-        with tf.GradientTape() as tape:
-            mlm_logits, nsp_logits = self.call(seqs, segs, training=True)
-            pad_mask = tf.math.not_equal(seqs_, self.padding_idx)
-            pred_loss = tf.reduce_mean(tf.boolean_mask(self.cross_entropy(seqs_, mlm_logits), pad_mask))
-            nsp_loss = tf.reduce_mean(self.cross_entropy(nsp_labels, nsp_logits))
-            loss = pred_loss + 0.2 * nsp_loss
-            grads = tape.gradient(loss, self.trainable_variables)
-            self.opt.apply_gradients(zip(grads, self.trainable_variables))
-        return loss, mlm_logits
-
-    def input_emb(self, seqs, segs):
-        return self.word_emb(seqs) + self.segment_emb(segs) + self.position_emb  # [n, step, dim]
-
+        self.opt.zero_grad()
+        mlm_logits, nsp_logits = self(seqs, segs, training=True)
+        pred_loss = cross_entropy(mlm_logits.reshape(-1,self.n_vocab),seqs_.reshape(-1))
+        nsp_loss = cross_entropy(nsp_logits,nsp_labels.reshape(-1))
+        loss = pred_loss + 0.2 * nsp_loss
+        loss.backward()
+        self.opt.step()
+        return loss.cpu().data.numpy(), mlm_logits
+    
+    def input_emb(self,seqs, segs):
+        # device = next(self.parameters()).device
+        # self.position_emb = self.position_emb.to(device)
+        return self.word_emb(seqs) + self.segment_emb(segs) + self.position_emb
+    
     def mask(self, seqs):
-        """
-         abcd--
-        a011111
-        b001111
-        c000111
-        d000011
-        -000011
-        -000011
-
-        force head not to see afterward. eg.
-        a is a embedding for a---
-        b is a embedding for ab--
-        c is a embedding for abc-
-        later, b embedding will + b another embedding from previous residual input to predict c
-        """
-        mask = 1 - tf.linalg.band_part(tf.ones((self.max_len, self.max_len)), -1, 0)
-        pad = tf.math.equal(seqs, self.padding_idx)
-        mask = tf.where(pad[:, tf.newaxis, tf.newaxis, :], 1, mask[tf.newaxis, tf.newaxis, :, :])
-        return mask  # (step, step)
-
+        device = next(self.parameters()).device
+        batch_size, seq_len = seqs.shape
+        mask = torch.triu(torch.ones((seq_len,seq_len), dtype=torch.long), diagonal=1).to(device)  # [seq_len ,seq_len]
+        pad = torch.eq(seqs,self.padding_idx)   # [n, seq_len]
+        mask = torch.where(pad[:,None,None,:],1,mask[None,None,:,:]).to(device)   # [n, 1, seq_len, seq_len]
+        return mask>0   # [n, 1, seq_len, seq_len]
+    
     @property
     def attentions(self):
         attentions = {
-            "encoder": [l.mh.attention.numpy() for l in self.encoder.ls],
+            "encoder": [l.mh.attention.cpu().data.numpy() for l in self.encoder.encoder_layers]
         }
         return attentions
 
+def train():
+    MODEL_DIM = 256
+    N_LAYER = 4
+    LEARNING_RATE = 1e-4
+    dataset = utils.MRPCData("./MRPC",2000)
+    print("num word: ",dataset.num_word)
+    model = GPT(
+        model_dim=MODEL_DIM, max_len=dataset.max_len-1, num_layer=N_LAYER, num_head=4, n_vocab=dataset.num_word,
+        lr=LEARNING_RATE, max_seg=dataset.num_seg, drop_rate=0.2, padding_idx=dataset.pad_id
+    )
+    if torch.cuda.is_available():
+        print("GPU train avaliable")
+        device =torch.device("cuda")
+        model = model.cuda()
+    else:
+        device = torch.device("cpu")
+        model = model.cpu()
+    
+    loader = DataLoader(dataset,batch_size=32,shuffle=True)
 
-def train(model, data, step=10000, name="gpt"):
-    t0 = time.time()
-    for t in range(step):
-        seqs, segs, xlen, nsp_labels = data.sample(16)
-        loss, pred = model.step(seqs[:, :-1], segs[:, :-1], seqs[:, 1:], nsp_labels)
-        if t % 100 == 0:
-            pred = pred[0].numpy().argmax(axis=1)
-            t1 = time.time()
-            print(
-                "\n\nstep: ", t,
-                "| time: %.2f" % (t1 - t0),
-                "| loss: %.3f" % loss.numpy(),
-                "\n| tgt: ", " ".join([data.i2v[i] for i in seqs[0, 1:][:xlen[0].sum()+1]]),
-                "\n| prd: ", " ".join([data.i2v[i] for i in pred[:xlen[0].sum()+1]]),
+    for epoch in range(100):
+        for batch_idx, batch in enumerate(loader):
+            seqs, segs,xlen,nsp_labels = batch
+            seqs, segs,nsp_labels = seqs.type(torch.LongTensor).to(device), segs.type(torch.LongTensor).to(device),nsp_labels.to(device)
+            # pred: [n, step, n_vocab]
+            loss,pred = model.step(seqs=seqs[:,:-1], segs= segs[:,:-1], seqs_=seqs[:,1:], nsp_labels=nsp_labels)
+            if batch_idx %100 == 0:
+                pred = pred[0].cpu().data.numpy().argmax(axis = 1) # [step]
+                print(
+                    "Epoch: ",epoch,
+                "|batch: ", batch_idx,
+                "| loss: %.3f" % loss,
+                "\n| tgt: ", " ".join([dataset.i2v[i] for i in seqs[0, 1:].cpu().data.numpy()[:xlen[0].sum()+1]]),
+                "\n| prd: ", " ".join([dataset.i2v[i] for i in pred[:xlen[0].sum()+1]]),
                 )
-            t0 = t1
-    os.makedirs("./visual/models/%s" % name, exist_ok=True)
-    model.save_weights("./visual/models/%s/model.ckpt" % name)
+    os.makedirs("./visual/models/gpt",exist_ok=True)
+    torch.save(model.state_dict(),"./visual/models/gpt/model.pth")
+    export_attention(model,device,dataset)
 
-
-def export_attention(model, data, name="gpt"):
-    model.load_weights("./visual/models/%s/model.ckpt" % name)
-
-    # save attention matrix for visualization
-    seqs, segs, xlen, nsp_labels = data.sample(32)
-    model.call(seqs[:, :-1], segs[:, :-1], False)
+def export_attention(model,device,data,name="gpt"):
+    model.load_state_dict(torch.load("./visual/models/gpt/model.pth",map_location=device))
+    seqs, segs,xlen,nsp_labels = data[:32]
+    seqs, segs,xlen,nsp_labels = torch.from_numpy(seqs),torch.from_numpy(segs),torch.from_numpy(xlen),torch.from_numpy(nsp_labels)
+    seqs, segs,nsp_labels = seqs.type(torch.LongTensor).to(device), segs.type(torch.LongTensor).to(device),nsp_labels.to(device)
+    model(seqs[:,:-1],segs[:,:-1],False)
+    seqs = seqs.cpu().data.numpy()
     data = {"src": [[data.i2v[i] for i in seqs[j]] for j in range(len(seqs))], "attentions": model.attentions}
     path = "./visual/tmp/%s_attention_matrix.pkl" % name
     os.makedirs(os.path.dirname(path), exist_ok=True)
     with open(path, "wb") as f:
         pickle.dump(data, f)
-
-
 if __name__ == "__main__":
-    utils.set_soft_gpu(True)
-    MODEL_DIM = 256
-    N_LAYER = 4
-    LEARNING_RATE = 1e-4
+    train()
+            
 
-    d = utils.MRPCData("./MRPC", 2000)
-    print("num word: ", d.num_word)
-    m = GPT(
-        model_dim=MODEL_DIM, max_len=d.max_len - 1, n_layer=N_LAYER, n_head=4, n_vocab=d.num_word,
-        lr=LEARNING_RATE, max_seg=d.num_seg, drop_rate=0.2, padding_idx=d.pad_id)
-    train(m, d, step=5000, name="gpt")
-    export_attention(m, d, name="gpt")
+
 
