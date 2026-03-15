1cf9d8268a6ebcd0b0e06aa15000bdefe74f177e
cj <hdchenjian@163.com>
2026-03-15 22:05:12 +0800

    fix typo

--- rl/mofanpy/00_Q_learling_search.py
+++ rl/mofanpy/00_Q_learling_search.py
@@ -2,7 +2,6 @@
 A simple example for Reinforcement Learning using table lookup Q-learning method.
 An agent "o" is on the left of a 1 dimensional world, the treasure is on the rightmost location.
 Run this program and to see how the agent will improve its strategy of finding the treasure.
-
 https://mofanpy.com/tutorials/machine-learning/reinforcement-learning/general-rl
 """
 
