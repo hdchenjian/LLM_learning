347b14ef9f633fe908a8c96ab03c3450972a76e9
cj <hdchenjian@163.com>
2026-03-26 18:36:26 +0800

    fix typo

--- train/llama2.c/happy_llm_chapter5/dataset.py
+++ train/llama2.c/happy_llm_chapter5/dataset.py
@@ -104,3 +104,4 @@ class SFTDataset(Dataset):
         Y = np.array(input_id[1:]).astype(np.int64)
         loss_mask = np.array(loss_mask[1:]).astype(np.int64)
         return torch.from_numpy(X), torch.from_numpy(Y), torch.from_numpy(loss_mask)
+
